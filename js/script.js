34e3f3be0dc0fe8a60858617142174b5f3f3f034
Marina <marina.adamenko@lamp-dev.com>
2021-08-14 17:35:54 +0300

    added cart info

--- js/script.js
+++ js/script.js
@@ -36,3 +36,34 @@ function sortFunction(){
         }
     });
 }
+
+function countSum(price, number) {
+    price = parseInt(price);
+    number = parseInt(number);
+    let cartNumber = localStorage.getItem('cartNumber') ? parseInt(localStorage.getItem('cartNumber')) : 0;
+    let cartSum = localStorage.getItem('cartSum') ? parseInt(localStorage.getItem('cartSum')) : 0;
+    localStorage.setItem('cartNumber', cartNumber + number);
+    localStorage.setItem('cartSum', price * number + cartSum);
+    document.getElementById('total_number').innerHTML = cartNumber + number;
+    document.getElementById('total_sum').innerHTML = price * number + cartSum;
+}
+
+function handler(el){
+    const target = el.target;
+
+    if (target.className != 'product-box__btn') {
+        return;
+    }
+
+    let item = target.closest("div").querySelector('.qty__item');
+    let number = item.value;
+
+    if (!number || number == 0) {
+        return;
+    }
+
+    let price = parseInt(item.getAttribute('data-price'));
+    countSum(item.getAttribute('data-price'), item.value);
+}
+
+document.addEventListener("click", handler, false);
