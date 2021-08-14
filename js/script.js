ea5290c8aff685e6ebb060f5c2ee17bb58e36790
Marina <marina.adamenko@lamp-dev.com>
2021-08-14 20:05:14 +0300

    added make order modal

--- js/script.js
+++ js/script.js
@@ -48,8 +48,52 @@ function countSum(price, number) {
     document.getElementById('total_sum').innerHTML = price * number + cartSum;
 }
 
+function makeOrder() {
+    if (!localStorage.getItem('cartNumber')) {
+        alert('В корзине нет ни одного товара.');
+        return;
+    }
+    document.getElementById('modal_check').classList.add('active');
+    
+    document.getElementById('modal_close').addEventListener("click", () => {
+        document.getElementById('modal_check').classList.remove('active');
+    });
+
+    document.getElementById('btn_send_order').addEventListener("click", () => {
+        let customerName = document.getElementById("customer_name").value;
+        let customerEmail = document.getElementById("customer_email").value;
+        let error = '';
+        if (!customerName.match('^[a-zA-Zа-яёА-ЯЁ][a-zA-Zа-яёА-ЯЁ]+$')) {
+            alert('Имя должно содержить только буквы!');
+            error++;
+        }
+
+        if (!customerEmail.match('^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$')) {
+            alert('Поверьте правильность написания емейла!');
+            error++;
+        }
+
+        if (!error) {
+            //there should be a form submit by ajax
+            document.getElementById('modal_check').classList.remove('active');
+            alert('Благодарим за покупки!');
+            localStorage.clear();
+            document.getElementById('total_number').innerHTML = document.getElementById('total_sum').innerHTML = 'XXX';
+            let inputs = document.querySelectorAll('input');
+            for (let i = 0;  i < inputs.length; i++) {
+                inputs[i].value = '';
+            };
+        }
+    });
+}
+
 function handler(el){
     const target = el.target;
+
+    if (target.id == 'btn_modal_check') {
+        makeOrder();
+        return;
+    }
 
     if (target.className != 'product-box__btn') {
         return;
@@ -62,7 +106,6 @@ function handler(el){
         return;
     }
 
-    let price = parseInt(item.getAttribute('data-price'));
     countSum(item.getAttribute('data-price'), item.value);
 }
 
